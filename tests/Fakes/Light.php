8e3bc6c0ea42d63d581d64c9d3308e924a1f44dd
Christian Griffin <c.griffin@vehikl.com>
2019-07-12 09:44:00 -0400

    Add fake Light class for tests.

---
+++ tests/Fakes/Light.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace PhpTrafficLight\Tests\Fakes;
+
+use PhpTrafficLight\Interfaces\LightInterface;
+use PHPUnit\Framework\Assert;
+
+class Light implements LightInterface
+{
+    /**
+     * @const int
+     */
+    const LIGHT_OFF = 0;
+
+    /**
+     * @const int
+     */
+    const LIGHT_ON = 1;
+
+    /**
+     * @var int
+     */
+    private $state = self::LIGHT_OFF;
+
+    /**
+     * @return bool
+     */
+    public function activate(): bool
+    {
+        $this->state = self::LIGHT_ON;
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function deactivate(): bool
+    {
+        $this->state = self::LIGHT_OFF;
+        return true;
+    }
+
+    /**
+     * @return void
+     */
+    public function assertOff(): void
+    {
+        Assert::assertEquals(self::LIGHT_OFF, $this->state, 'Failed asserting that light is off.');
+    }
+
+    /**
+     * @return void
+     */
+    public function assertON(): void
+    {
+        Assert::assertEquals(self::LIGHT_ON, $this->state, 'Failed asserting that light is on.');
+    }
+}
