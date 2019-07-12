634a4d8245c8a8ea8c5335dd5aed3d40270c41e4
Christian Griffin <c.griffin@vehikl.com>
2019-07-12 09:36:09 -0400

    Fix TrafficLightTest instantiation test name.

---
+++ tests/Unit/TrafficLightTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace PhpTrafficLight\Tests\Unit;
+
+use PhpTrafficLight\Light;
+use PhpTrafficLight\Tests\TestCase;
+use PhpTrafficLight\Tests\Fakes\TrafficLightApi;
+use PhpTrafficLight\TrafficLight;
+
+final class TrafficLightTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testItCanBeInstantiatedWithThreeLights(): void
+    {
+        $api = new TrafficLightApi;
+        $this->assertInstanceOf(
+            TrafficLight::class,
+            new TrafficLight(
+                new Light('red', $api),
+                new Light('yellow', $api),
+                new Light('green', $api)
+            )
+        );
+    }
+}
